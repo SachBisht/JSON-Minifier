e04bcf21c3412fd27fd83e3ef653d30d87013e8a
SachBisht <38837385+SachBisht@users.noreply.github.com>
2019-01-25 18:45:05 +0530

    Added the whole Project
    Add the Package:
    Newtonsoft.Json.12.0.1/Newtonsoft.Json.12.0.1.nupkg

---
+++ JSON_Minifier/CoastalDistance/GeoCoordinates.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace CoastalDistance
+{
+    class GeoCoordinates : Coordinate
+    {
+        public static void GetCoordinates()
+        {
+
+            Coordinate coordinate = new Coordinate();
+
+            List<string> subCoordinates = new List<string>();
+            var subSubCoordinates = new List<List<string>>();
+
+            //For JSON READING
+            using (StreamReader file = File.OpenText(@"D:\Southeast_Caribbean_1.json"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                JObject obj = JObject.Load(reader);
+                dynamic data = JObject.Parse(obj.ToString());
+                for (int j = 0; j < data.features.Count; j++)
+                {
+                    for (int i = 0; i < data.features[j].geometry["coordinates"].Count;)
+                    {
+                        double latCoordinate = (double)(data.features[j].geometry["coordinates"][i][1]);
+                        double longCoordinate = (double)(data.features[j].geometry["coordinates"][i][0]);
+                        subCoordinates.Add(latCoordinate.ToString());
+                        subCoordinates.Add(longCoordinate.ToString());
+                        if(data.features[j].geometry["coordinates"].Count>20)
+                        {
+                            i = i + 5;
+                        }
+                        else
+                        {
+                            i = i + 2;
+                        }
+                    }
+
+                }
+                subSubCoordinates.Add(subCoordinates);
+                coordinate.Coordinates = subSubCoordinates;
+
+                BinaryFormatter formatter = new BinaryFormatter();
+                StreamWriter sw = new StreamWriter(@"D:\path.json");
+                JsonSerializer jsonSerializer = JsonSerializer.Create();
+                jsonSerializer.Serialize(sw,coordinate.Coordinates);
+                sw.Close();
+                
+
+                //JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                //javaScriptSerializer.MaxJsonLength = 999999999;
+                //string jsondata = javaScriptSerializer.Serialize(coordinate);
+                //System.IO.File.WriteAllText(@"D:\path.json", jsondata);
+                Console.WriteLine("Done!!");
+            }
+        }
+    }
+
+}
+
+///Previous One
+///
+//public static void GetCoordinates()
+//{
+
+//    //FOR JSON WRITING
+
+//    Coordinate coordinate = new Coordinate();
+
+//    List<string> subCoordinates = new List<string>();
+//    var subSubCoordinates = new List<List<string>>();
+
+//    string json = "";
+
+//    //For JSON READING
+//    using (StreamReader file = File.OpenText(@"D:\SBisht\Coastline_Project\Info\Coastline_CoordinateValues.json"))
+//    using (JsonTextReader reader = new JsonTextReader(file))
+//    {
+//        JObject obj = JObject.Load(reader);
+//        dynamic data = JObject.Parse(obj.ToString());
+//        string startsAs = "{\"Coordinates\":[";
+//        subCoordinates.Add(startsAs);
+//        for (int j = 0; j < data.geometries.Count; j++)
+//        {
+//            for (int i = 0; i < data.geometries[j].coordinates.Count; i++)
+//            {
+//                double latCoordinate = (double)(data.geometries[j].coordinates[i][1]);
+//                double longCoordinate = (double)(data.geometries[j].coordinates[i][0]);
+
+//                string temp = "[" + longCoordinate + "," + latCoordinate + "]";
+//                subCoordinates.Add(temp);
+
+
+
+//            }
+
+//        }
+//        string endsAs = "]}";
+//        subCoordinates.Add(endsAs);
+//        subSubCoordinates.Add(subCoordinates);
+//        string jsondata = new JavaScriptSerializer().Serialize(coordinate);
+//        json = JsonConvert.SerializeObject(subSubCoordinates.ToArray());
+//        System.IO.File.WriteAllText(@"D:\path.json", json);
+//        Console.WriteLine("Done!!");
---
+++ JSON_Minifier/CoastalDistance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+////Used Newtonsoft.JSON library for file reading!!
+//31.0984679,-81.385627  ============Provided Ones
+//33.8486573,-77.9752082
+//1 mile = 5280 feet
+
+namespace CoastalDistance
+{
+    class Program : GeoCoordinates
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            
+            GetCoordinates();
+            Console.ReadKey();
+        }
+
+    }
+}
